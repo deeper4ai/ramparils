3f5a7f2718ba1bc8d5bb09b0b65315c3214b8a57
Jan Jakubuv <jakubuv@gmail.com>
2026-08-21 08:48:23 +0200

    docs: update gh-pages
    Source: 644c946740ceec99bdc6acbb00f6adfc503cb1c5

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-03f54fae.js',
+                path_to_root + 'searchindex-85a43fb3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
